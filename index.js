67b826719a6a3914d1f2debda3beae38cad4e5ee
ashpak <ashapakshaikh1@gmail.com>
2018-05-27 22:29:18 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+
+  app.get('/', function (req, res) {
+   res.sendfile(__dirname + '/views/home.html');
+
+
+  })
+
+
+app.listen(3000)
+
